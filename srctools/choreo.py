05c8874e18401f583d8af4b0345980c4d9d860bf
Spencer Brown <spencerb21@live.com>
2021-06-30 18:59:26 +1000

    Handle version 3 scenes.image format

--- srctools/choreo.py
+++ srctools/choreo.py
@@ -13,8 +13,29 @@ class Choreo:
     """A choreographed scene."""
     filename: str  # Filename if available.
     checksum: CRC  # CRC hash.
-    duration: int  # Duration in milliseconds.
+    duration_ms: int  # Duration in milliseconds.
+    last_speak_ms: int  # Time at which the last voice line ends.
     sounds: List[str]  # List of sounds it uses.
+
+    @property
+    def duration(self) -> float:
+        """Return the duration in seconds."""
+        return self.duration_ms / 1000.0
+
+    @duration.setter
+    def duration(self, value: float) -> None:
+        """Set the duration (in seconds). This is rounded to the nearest millisecond."""
+        self.duration_ms = round(value * 1000.0)
+
+    @property
+    def last_speak(self) -> float:
+        """Return the last-speak time in seconds."""
+        return self.last_speak_ms / 1000.0
+
+    @last_speak.setter
+    def last_speak(self, value: float) -> None:
+        """Set the last-speak time (in seconds). This is rounded to the nearest millisecond."""
+        self.last_speak_ms = round(value * 1000.0)
 
 
 def parse_scenes_image(file: IO[bytes]) -> Dict[CRC, Choreo]:
@@ -28,7 +49,7 @@ def parse_scenes_image(file: IO[bytes]) -> Dict[CRC, Choreo]:
     ] = struct_read('<4s4i', file)
     if magic != b'VSIF':
         raise ValueError("Invalid scenes.image!")
-    if version != 2:
+    if version not in (2, 3):
         raise ValueError("Unknown version {}!".format(version))
 
     # Read the indexes in order from the file, then read the null-terminated
@@ -42,22 +63,24 @@ def parse_scenes_image(file: IO[bytes]) -> Dict[CRC, Choreo]:
     scenes: dict[CRC, Choreo] = {}
 
     file.seek(scene_off)
-    scene_data: List[Tuple[CRC, int]] = [()] * scene_count
+    scene_data: List[Tuple[CRC, int, int, int]] = [()] * scene_count
     for i in range(scene_count):
-        [
-            crc,
-            data_off,
-            data_size,
-            summary_off,
-        ] = struct_read('<4i', file)
-        scene_data[i] = (crc, summary_off)
+        scene_data[i] = struct_read('<4i', file)
 
-    for crc, summary_off in scene_data:
+    for (
+        crc,
+        data_off, data_size,
+        summary_off,
+    ) in scene_data:
         file.seek(summary_off)
-        [duration, sound_count] = struct_read('<2i', file)
+        if version == 3:
+            [duration, last_speak, sound_count] = struct_read('<3i', file)
+        else:
+            [duration, sound_count] = struct_read('<2i', file)
+            last_speak = duration - 1  # Assume it's the whole choreo.
         sounds = [
             string_pool[i]
             for i in struct_read('<{}i'.format(sound_count), file)
         ]
-        scenes[crc] = Choreo('', crc, duration, sounds)
+        scenes[crc] = Choreo('', crc, duration, last_speak, sounds)
     return scenes
